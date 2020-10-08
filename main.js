2aba3e3f66a7a6b01c205763de66822d5e40f54a
SaiAnshuman <71771487+SaiAnshuman@users.noreply.github.com>
2020-10-08 18:40:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Hey This Is My Console Project :D");
+
+var first = "Mine";
+var second = "Craft";
+var joined = first + second;
+console.log(joined);
+
+var number1 = "4";
+var number2 = "1";
+var addition = number1 + number2;
+var subtraction = number1 - number2;
+var multiplication = number1 * number2;
+var division = number1 / number2;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
